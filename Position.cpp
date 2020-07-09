4f3f81cde35dd1554e2a92ba58efab82e6b18c25
Domanat <maxim.tomilko@yandex.ru>
2020-07-09 13:51:46 +0300

    Add implementations for functions

--- Position.cpp
+++ Position.cpp
@@ -1,1 +1,23 @@
 #include "Position.h"
+
+Position::Position(const std::string name, int id, int count)
+{
+	this->name = name;
+	this->id = id;
+	this->count = count;
+}
+
+std::string Position::getName() const
+{
+	return name;
+}
+
+int Position::getId() const
+{
+	return id;
+}
+
+int Position::getCount() const
+{
+	return count;
+}
