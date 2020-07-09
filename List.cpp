59bab8e8fd3859680fede9dcf7e1a1ab7fb1af6c
Domanat <maxim.tomilko@yandex.ru>
2020-07-09 14:04:44 +0300

    Implementations of list functions

---
+++ List.cpp
@@ -0,0 +1,26 @@
+#include "List.h"
+#include <iostream>
+
+void List::addPosition(const std::string name, int id, int count)
+{
+	list.push_back(Position(name, id, count));
+}
+
+void List::deletePosition(const std::string name)
+{
+	auto delPosition = std::find_if(list.begin(), list.end(), [name](const Position& p) {
+		return p.getName() == name;
+	});
+
+	list.erase(delPosition);
+}
+
+void List::printAll()
+{
+	for (auto i : list)
+	{
+		std::cout << "______________________________________________" << std::endl;
+		std::cout << "Name: " << i.getName() << "\nID: " << i.getId() << "\nCount: " << i.getCount() << std::endl;
+		std::cout << "______________________________________________" << std::endl;
+	}
+}
