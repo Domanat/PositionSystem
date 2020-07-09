a92efc619c19ff8f95c59de141042406669dd4e5
Domanat <maxim.tomilko@yandex.ru>
2020-07-09 13:51:24 +0300

    Add getter functions

--- Position.h
+++ Position.h
@@ -6,7 +6,11 @@
 class Position
 {
 public: 
-	Position(const std::string name, int count, int id);
+	Position(const std::string name, int id, int count);
+
+	std::string getName() const;
+	int getCount() const;
+	int getId() const;
 
 private:
 	std::string name;
