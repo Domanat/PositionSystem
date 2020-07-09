be58685c60c569c4d530f472ac40c2e619060c92
Domanat <maxim.tomilko@yandex.ru>
2020-07-09 13:39:00 +0300

    Add name, id, count members to the class

--- Position.h
+++ Position.h
@@ -1,10 +1,17 @@
 #ifndef POSITION_H
 #define POSITION_H
 
+#include <string>
+
 class Position
 {
 public: 
+	Position(const std::string name, int count, int id);
 
+private:
+	std::string name;
+	int count;
+	int id;
 };
 
 #endif
